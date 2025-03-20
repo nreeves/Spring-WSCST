c581e89bb9d851180b4d9682a97ee0d9bd6e03b2
Nathan Reeves <nreeves.em@gmail.com>
2025-03-20 16:45:46 -0500

    h

--- assignment5/assignment5/src/App.js
+++ assignment5/assignment5/src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponents';
-import { DISHES } from './shared/dishes'; // Import the DISHES data
+import Main from './components/MainComponents';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES // Store DISHES in state
-    };
-  }
-
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} /> {/* Pass dishes as props */}
+        <Main />
       </div>
     );
   }
