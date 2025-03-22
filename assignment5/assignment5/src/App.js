1f12390e9ff6a889850e36783a1f39629975aef9
Nathan Reeves <nreeves.em@gmail.com>
2025-03-22 16:44:52 -0500

    prepping for project 7
    making sure less css works, making sure to integrate the html project into react, etc

---
+++ assignment5/assignment5/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Main />
+            </div>
+        );
+    }
+}
+
+export default App;
